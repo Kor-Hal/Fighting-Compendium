31509035012a9e16865db1172114bc74dc8ba2f1
Khoral <Khoral@users.noreply.github.com>
2017-09-12 09:57:50 +0200

    MaJ modèle BdD
    Mise à jour des inserts et de la requête exemple suite à la modification du modèle BdD

--- ProjectDocuments/Requete_exemple.sql
+++ ProjectDocuments/Requete_exemple.sql
@@ -1,7 +1,7 @@
-select CH.name AS Character, S.notation AS State, C.notation AS Command, CT.symbol AS Link
-FROM Inputs I LEFT OUTER JOIN States S ON I.idState = S.idState
+select CH.name AS Character, S.notation AS State, C.notation AS Command, CT.symbolPath AS Link
+FROM Inputs I LEFT OUTER JOIN States S ON I.idStartState = S.idState
 LEFT OUTER JOIN Commands C ON I.idCommand = C.idCommand
 LEFT OUTER JOIN CombinationTypes CT ON I.idCombinationType = CT.idCombinationType
 JOIN Moves M ON I.idMove = M.idMove
 JOIN Characters CH ON M.idCharacter = CH.idCharacter
-ORDER BY I.ordering
+ORDER BY I.ordering;
